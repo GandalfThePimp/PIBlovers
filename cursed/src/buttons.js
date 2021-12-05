0681ed5335b137a938cc5e1b0fbfe81505fc2271
croi <cristianalexandrucroitoru@gmail.com>
2021-12-05 04:54:25 +0200

    navar

--- cursed/src/buttons.js
+++ cursed/src/buttons.js
@@ -1,5 +1,22 @@
 function login_button(){
-    user_exists_async(document.getElementById('username').vlaue).then((res)=>{
-        JSON.parse(res).exists == 1;
-    });
+    user_exists_async(document.getElementById('username').value).then((res)=>{
+        var v = JSON.parse(res).exists;
+        console.log(res);
+        if(v == 1){
+            //goto welcome_login
+            document.location = "welcome_login.html";
+        }else{
+            document.getElementById("loginbtn").remove();
+        }
+    }).catch((err)=>{})
+}
+
+function signup_button(){
+    api_login(document.getElementById('username').value,document.getElementById('password').value);
+}
+
+function noacc_button(){
+    //goto no acc
+    document.location = "welcome_noacc.html";
+
 }
